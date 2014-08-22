e42f2642f798982c422ff26a21e74141b78c24bd
iesfausti <fausti.informatica@gmail.com>
2014-08-22 17:49:55 +0200

    :neckbeard: Added .gitattributes & .gitignore files

---
+++ faustilib/test.py
@@ -0,0 +1,7 @@
+import os, sys, time, zipfile
+from datetime import datetime
+
+
+class test():
+	def ahora():
+		print(datetime.now())
---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'faustilib',
+  packages = ['faustilib'], # this must be the same as the name above
+  version = '0.1',
+  description = 'Una biblioteca de codigo del IES Fausti',
+  author = 'Fausti Informatica',
+  author_email = 'fausti.informatica@gmail.com',
+  url = ' https://github.com/iesfausti/faustilib', # use the URL to the github repo
+  download_url = ' https://github.com/iesfausti/faustilib/tarball/0.1', # I'll explain this in a second
+  keywords = ['educacion', 'secundaria', 'fausti'], # arbitrary keywords
+  classifiers = [],
+)
